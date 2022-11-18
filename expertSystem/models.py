f0a5f4cbfee0fc9f2a32d27b6971cf865d148687
Iván Mota <ivan_mota09@hotmail.com>
2022-11-17 23:30:46 -0600

    strs

--- expertSystem/models.py
+++ expertSystem/models.py
@@ -6,10 +6,16 @@ class Question(models.Model):
     id = models.AutoField(primary_key=True)
     label = models.CharField(max_length=100, blank=True, null=True)
 
+    def __str__(self):
+        return str(self.id) + '-' + self.label
+
 class Option(models.Model):
     id = models.AutoField(primary_key=True)
     label = models.CharField(max_length=100, blank=True, null=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='options')
+
+    def __str__(self):
+        return str(self.id) + '-' + self.label
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
@@ -17,3 +23,6 @@ class Car(models.Model):
     reason = models.TextField(blank=True, null=True)
     image = models.ImageField(blank=True, null=True)
     options = models.ManyToManyField(Option, related_name='cars')
+
+    def __str__(self):
+        return str(self.id) + '-' + self.response
